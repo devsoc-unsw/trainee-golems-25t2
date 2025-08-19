71df8f41b0ff74f08902d69cea2c11fcc8da5798
Froxzen <harvardchong1@gmail.com>
2025-08-19 15:56:28 +1000

    Changed header in Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -55,7 +55,7 @@ function Home() {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
-            Welcome to MyApp
+            Your Student Assistant
           </h1>
           {icons.map(({ src, posx, posy, rotate }, index) => (
             <FloatingIcon
