604348edbfadf4fccc345abb582743f2b15b398c
Yumna Zaheed <yumnazowfir@gmail.com>
2025-08-29 09:15:48 +1000

    added landing page and ball pit

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Landing from "./pages/Landing";
+
 
 export const Router = () => {
   return (
     <Routes>
+      <Route path="/" element={<Landing />} />
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
     </Routes>
