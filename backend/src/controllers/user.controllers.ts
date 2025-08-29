2f02a38349909490c3c0bb4611e621ccee8e09cd
Danar Prakosa <danar.prakosa@gmail.com>
2025-08-30 05:20:59 +1000

    add user profile endpoint

---
+++ backend/src/controllers/user.controllers.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { ErrorMap, StatusCodeMap } from "../constants/errors";
+import * as authService from "../services/auth.service";
+
+async function getUserProfile(req: Request, res: Response) {
+  try {
+    const sessionId = req.cookies.sessionId;
+    const user = await authService.getUserBySession(sessionId);
+    if (!user) {
+      throw new Error(ErrorMap.USER_DOES_NOT_EXIST);
+    }
+    res.json({
+      username: user.name,
+      email: user.email,
+      avatar: "profile_pic_url",
+    });
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : "An error occurred";
+    const statusCode = StatusCodeMap[message] || 500;
+    res.status(statusCode).json({
+      error: message,
+    });
+  }
+}
+
+export { getUserProfile }
