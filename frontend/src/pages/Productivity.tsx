b0ba063a0ddbebbf7293b40f28c8aa2f21cf9f4b
Danar Prakosa <danar.prakosa@gmail.com>
2025-09-06 16:40:40 +1000

    add blank productivity page

---
+++ frontend/src/pages/Productivity.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Timer from '../Components/Timer';
+
+const Productivity: React.FC = () => {
+    return (
+        <div>
+            <Timer />
+        </div>
+    );
+};
+
+export default Productivity;
