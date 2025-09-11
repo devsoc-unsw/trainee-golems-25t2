b67585460844a16f97a1ba170f524fa92635b977
Harvard Chong <harvardchong1@gmail.com>
2025-09-11 11:48:54 +1000

    Removed duplicate files

--- frontend/src/contexts/SidebarContextValue 3.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export interface SidebarContextType {
-  collapsed: boolean;
-  setCollapsed: (collapsed: boolean) => void;
-  toggleCollapsed: () => void;
-}
-
-export const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
